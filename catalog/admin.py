27263e1804d14b3824b5a55f6e4c70557203c514
amitmodi06 <amitmodi06@gmail.com>
2023-11-17 22:06:59 +0000

    Register the ModelAdmin class to get hold of how models will be viewed in admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 from .models import Book, Author, Genre,  BookInstance, Language
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(BookInstance)
+
+# admin.site.register(Book)
+@admin.register(Book)  # this decorator is another way of writing admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
 admin.site.register(Genre)
-admin.site.register(Author)
 admin.site.register(Language)
