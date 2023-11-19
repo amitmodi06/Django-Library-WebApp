bf7638461242096576554d8eb96ea6975841a42e
amitmodi06 <amitmodi06@gmail.com>
2023-11-19 19:35:50 +0000

    added inline listing of the Book items to the Author

--- catalog/admin.py
+++ catalog/admin.py
@@ -18,6 +18,10 @@ class BookInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
 
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
 @admin.register(Book)  # this decorator is another way of writing admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
@@ -45,6 +49,8 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'data_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'data_of_death')]
+
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
