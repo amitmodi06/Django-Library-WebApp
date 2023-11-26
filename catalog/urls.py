f7fa1064ca665d925dbd190b6709b80521262226
amitmodi06 <amitmodi06@gmail.com>
2023-11-26 19:35:22 +0000

    added book renew functionality that only librarian user can renew the book. FOr this created form to renew, added url, created a view function and created a html template for the form.

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,14 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),    
+]
+
+urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,18 @@
+import datetime
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book, BookInstance, Author, Genre, Language
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from catalog.forms import RenewBookForm
+
 
 # Create your views here.
 def index(request):
@@ -80,8 +88,8 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
 class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to all user.
         Only visible to users with 'can_mark_returned' permission"""
-    permission_required = 'catalog.can_mark_returned'
     model = BookInstance
+    permission_required = 'catalog.can_mark_returned'    
     template_name = 'catalog/bookinstance_list_all_borrowed.html'
     paginate_by = 10
 
@@ -90,3 +98,37 @@ class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
             BookInstance.objects.filter(status__exact='o')
             .order_by('due_back')
         )
+    
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
